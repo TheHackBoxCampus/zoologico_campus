8982a56f2a4ea40a3a8670f99ca4db29b87c2471
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-08-29 00:47:41 -0500

    feat(root): Realizacion de endpoints
    Se realizaron 5 endpoints
    
    - /especies
    - /animales
    - /animales/:id
    -/especie?
    - /cantidad/especie
    
    Se creo la logica y se distribuyo en versiones. Claramente la logica esta por separado, los controladores en un lado, los routers en otro, las configuraciones de las versiones en otro. Para el manejo de un buen orden.

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 import express from "express"; 
+import Router_animal from "./routers/[animal].js";
+import Router_specie from "./routers/[specie].js";
 
 const app = express(); 
 
@@ -9,5 +11,12 @@ const app = express();
 app.use(express.json()); 
 app.use(express.text());
 
+
+/**
+ * ! Routers
+ */
+
+app.use(Router_animal); 
+app.use(Router_specie); 
 
 export default app
