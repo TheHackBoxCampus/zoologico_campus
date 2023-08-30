c9f80cc5225a81a61ddcbbb8ea49d01d7ace9324
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-08-30 14:08:37 -0500

    feat(root): Carga de archivos a main

--- src/controllers/v1/auth.controller.js
+++ src/controllers/v1/auth.controller.js
@@ -1,1 +1,30 @@
 import { conx } from "../../config/db.js";
+import { generateToken } from "../../libs/generateToken.js";
+
+const verifyid = async (user) => {
+    try {
+        let db = await conx(); 
+        let employeesCollection = await db.collection("visitante"); 
+        let visitorsCollection = await db.collection("")
+        let employee = await employeesCollection.findOne({email: user.email})
+
+        if(!employee) {
+            
+        } 
+
+    }catch(err) {
+        console.log(err)
+    }
+}
+
+const login = async (req, res) => {
+    try {
+        let user = req.body; 
+ 
+
+        
+
+    }catch(err) {
+        console.log(err)
+    }
+}
